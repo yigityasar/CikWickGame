b772e2b18cbc4bdaa634d3eff6d0300a561f4eb6
Yiğit Yaşar <yigityasar.dev@gmail.com>
2025-09-05 15:16:56 +0300

    v0.3 Character Controller Added

---
+++ Assets/_GameAssets/Scripts/PlayerController.cs
@@ -0,0 +1,126 @@
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform _orientationTransform;
+
+    [Header("Movement Settings")]
+    [SerializeField] private KeyCode _movementKey;
+    [SerializeField] private float _movementSpeed = 20f;
+
+    [Header("Jump Settings")]
+    [SerializeField] private KeyCode _jumpKey;
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private float _jumpCooldown;
+    [SerializeField] private bool _canJump;
+
+    [Header("Sliding Settings")]
+    [SerializeField] private KeyCode _slideKey;
+    [SerializeField] private float _slideMultiplier;
+    [SerializeField] private float _slideDrag;
+
+    [Header("Ground Check Settings")]
+    [SerializeField] private float _playerHeight;
+    [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private float _groundDrag;
+
+
+
+
+    private Rigidbody _playerRigidbody;
+    private float _horizontalInput, _verticalInput;
+    private Vector3 _movementDirection;
+    private bool _isSliding;
+
+    private void Awake()
+    {
+        _playerRigidbody = GetComponent<Rigidbody>();
+        _playerRigidbody.freezeRotation = true;
+    }
+    private void Update()
+    {
+        SetInputs();
+        SetPlayerDrag();
+        LimitPlayerSpeed();
+    }
+    private void FixedUpdate()
+    {
+        SetPlayerMovement();
+    }
+    private void SetInputs()
+    {
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+        _verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(_slideKey))
+        {
+            _isSliding = true;
+            Debug.Log("Player Sliding!");
+        }
+
+        else if (Input.GetKeyDown(_movementKey))
+        {
+            _isSliding = false;
+            Debug.Log("Player Moving!");
+        }
+
+        else if (Input.GetKey(_jumpKey) && _canJump && IsGrounded()) //zıplama işlemi gerçekleşecek
+        {
+            _canJump = false;
+            SetPlayerJumping();
+            Invoke(nameof(ResetJumping), _jumpCooldown);
+
+        }
+    }
+
+    private void SetPlayerMovement()
+    {
+        _movementDirection = _orientationTransform.forward * _verticalInput + _orientationTransform.right * _horizontalInput;
+
+        if (_isSliding)
+        {
+            _playerRigidbody.AddForce(_movementDirection.normalized * _movementSpeed * _slideMultiplier, ForceMode.Force);
+        }
+        else
+        {
+            _playerRigidbody.AddForce(_movementDirection.normalized * _movementSpeed, ForceMode.Force);
+        }
+    }
+    private void SetPlayerDrag()
+    {
+        if (_isSliding)
+            _playerRigidbody.linearDamping = _slideDrag;
+        else
+            _playerRigidbody.linearDamping = _groundDrag;
+    }
+
+    private void LimitPlayerSpeed()
+    {
+        Vector3 flatVelocity = new Vector3(_playerRigidbody.linearVelocity.x, 0f, _playerRigidbody.linearVelocity.z);
+
+        if (flatVelocity.magnitude > _movementSpeed)
+        {
+            Vector3 limitedVelocity = flatVelocity.normalized * _movementSpeed;
+            _playerRigidbody.linearVelocity = new Vector3(limitedVelocity.x, _playerRigidbody.linearVelocity.y, limitedVelocity.z);
+        }
+    }
+    private void SetPlayerJumping()
+    {
+        _playerRigidbody.linearVelocity = new Vector3(_playerRigidbody.linearVelocity.x, 0f, _playerRigidbody.linearVelocity.z);
+        _playerRigidbody.AddForce(transform.up * _jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJumping()
+    {
+        _canJump = true;
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, _playerHeight * 0.5f + 0.2f, _groundLayer);
+    }
+
+
+}
